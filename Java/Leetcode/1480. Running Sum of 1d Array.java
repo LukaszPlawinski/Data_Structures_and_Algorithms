a43e91c60056c552ca8903cbb2916e9a09853efc
Lukasz Plawinski <plawinski.lukasz@gmail.com>
2020-06-29 01:22:16 +0100

    exercise 1480 Leetcode

---
+++ Java/Leetcode/1480. Running Sum of 1d Array.java
@@ -0,0 +1,26 @@
+/*
+Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+Return the running sum of nums.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: [1,3,6,10]
+Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+Example 2:
+
+Input: nums = [1,1,1,1,1]
+Output: [1,2,3,4,5]
+Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
+*/
+
+int ans[] = new int[nums.length];
+int c=0;
+for(int n = 0;n<nums.length;n++){
+    c+=nums[n];
+    ans[n]=c;
+}
+return ans;
